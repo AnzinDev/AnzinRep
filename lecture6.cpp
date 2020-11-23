a9b4cb31f66d6c5ab5365104ab4c19f712de7882
AnzinDev <70850176+AnzinDev@users.noreply.github.com>
2020-11-23 18:34:10 +0300

    Create lecture6.cpp

---
+++ lecture6.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+
+int i = 0;
+mutex mtx1, mtx2;
+
+void addition(vector<int>& container)
+{
+	mtx1.lock();
+	this_thread::sleep_for(chrono::milliseconds(1));
+	mtx2.lock();
+	for (int i = 0; i < 10; i++)
+	{
+		cout << this_thread::get_id() << " add" << endl;
+		container[i] = i;
+	}
+	mtx1.unlock();
+	mtx2.unlock();
+}
+
+void read(vector<int>& container)
+{
+	mtx1.lock();
+	this_thread::sleep_for(chrono::milliseconds(1));
+	mtx2.lock();
+	for (int i = 0; i < 10; i++)
+	{
+		cout << this_thread::get_id() << "\t" << container[i] << endl;
+	}
+	mtx1.unlock();
+	mtx2.unlock();
+}
+
+int main()
+{
+	vector<int> container(10, -1);
+
+	thread t1(addition, ref(container));
+
+	thread t2(read, ref(container));
+
+	t1.join();
+	t2.join();
+
+	return 0;
+}
