607c2ca39365542a702b5e01ea35980a7d68505b
AnzinDev <70850176+AnzinDev@users.noreply.github.com>
2020-11-24 17:16:49 +0300

    Update lecture6.cpp

--- lecture6.cpp
+++ lecture6.cpp
@@ -6,33 +6,27 @@
 
 using namespace std;
 
-mutex mtx1, mtx2;
+mutex mtx;
 
 void addition(vector<int>& container)
 {
-	mtx1.lock();
+	const lock_guard<mutex> mtxLock(mtx);
 	this_thread::sleep_for(chrono::milliseconds(1));
-	mtx2.lock();
 	for (int i = 0; i < 10; i++)
 	{
 		cout << this_thread::get_id() << " add" << endl;
 		container[i] = i;
 	}
-	mtx1.unlock();
-	mtx2.unlock();
 }
 
 void read(vector<int>& container)
 {
-	mtx1.lock();
+	const lock_guard<mutex> mtxLock(mtx);
 	this_thread::sleep_for(chrono::milliseconds(1));
-	mtx2.lock();
 	for (int i = 0; i < 10; i++)
 	{
 		cout << this_thread::get_id() << "\t" << container[i] << endl;
 	}
-	mtx1.unlock();
-	mtx2.unlock();
 }
 
 int main()
@@ -40,7 +34,6 @@ int main()
 	vector<int> container(10, -1);
 
 	thread t1(addition, ref(container));
-
 	thread t2(read, ref(container));
 
 	t1.join();
@@ -48,3 +41,4 @@ int main()
 
 	return 0;
 }
+
