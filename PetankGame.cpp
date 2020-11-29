b0c8e09f356baefa75b9a3de6889af2e80444402
AnzinDev <70850176+AnzinDev@users.noreply.github.com>
2020-11-30 00:40:39 +0300

    Update PetankGame.cpp

--- PetankGame.cpp
+++ PetankGame.cpp
@@ -1,112 +1,228 @@
 /*
-предварительная версия программы без обраотки столкновений шариков
+предварительная версия программы без обработки столкновений шариков
 */
-
+//корректное отображения угла + установка угла + установка угла в шарик
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <ctime>
 #include <thread>
 #include <cstdlib>
 #include <chrono>
-#include <cmath>
+#include <cmath> 
 
 #define PI 3.14159265
 
 using namespace sf;
+Font font;
+
+class Ball
+{
+	float sX = 180.f, sY = 720.f;
+
+public:
+	CircleShape ball;
+	bool flag, step;
+
+	Ball(float rad)
+	{
+		ball.setRadius(rad);
+		ball.setRotation(-90);
+	}
+
+	void Move(float currtime, float azimuth, int v0)
+	{
+
+		ball.move(-((v0 - (2.2 * currtime * currtime / 2)) * sin((azimuth)* PI / 180)), -((v0 - (2.2 * currtime * currtime / 2)) * cos((azimuth)* PI / 180)));
+	}
+
+	void SetColor(int r, int g, int b, int a)
+	{
+		ball.setFillColor(Color(r, g, b, a));
+	}
+
+	void SetStartPosition()
+	{
+		ball.setPosition(sX, sY);
+	}
+
+	void SetKoshPosition()
+	{
+		ball.setPosition(192.f, 170.f);
+	}
+
+	bool CheckOut()
+	{
+		if ((ball.getPosition().x >= 400.f) || (ball.getPosition().x < -40.f) || (ball.getPosition().x < -40.f))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool CheckLowline(float& lowline)
+	{
+		if (ball.getPosition().x < lowline) return true;
+		else return false;
+	}
+
+	float CurrVelocity(int v0, float currtime)
+	{
+		return v0 - (2.2 * currtime * currtime / 2);
+	}
+
+	float distance(Ball & otherBall)
+	{
+		return sqrt(pow((ball.getPosition().x) - (otherBall.ball.getPosition().x), 2) + pow((ball.getPosition().y) - (otherBall.ball.getPosition().y), 2));
+	}
+};
+
+class Azimuth
+{
+public:
+	int azimuth = 0;
+	RectangleShape AzLine;
+
+	Azimuth()
+	{
+		AzLine.setSize(Vector2f(50.f, 1.f));
+		AzLine.setPosition(450.f, 750.f);
+		AzLine.setFillColor(Color(255, 255, 255, 255));
+	}
+
+	int SetAzimuth(int azimuth)
+	{
+		this->azimuth = -azimuth - 90;
+		AzLine.setRotation(-azimuth - 90);
+		return azimuth;
+	}
+
+	int GetAzimuth()
+	{
+		return this->azimuth;
+	}
+};
+
+class StaticFigures
+{
+public:
+	RectangleShape SFigure;
+
+	StaticFigures(float X, float Y, int r, int g, int b, int a, float sX, float sY)
+	{
+		SFigure.setSize(Vector2f(X, Y));
+		SFigure.setFillColor(Color(r, g, b, a));
+		SFigure.setPosition(sX, sY);
+	}
+};
+
+class ForceStage
+{
+public:
+	ConvexShape stage;
+
+	ForceStage()
+	{
+		stage.setPointCount(4);
+		stage.setPoint(0, Vector2f(500.f, 780.f));
+		stage.setPoint(1, Vector2f(520.f, 780.f));
+		stage.setPoint(2, Vector2f(520.f, 780.f));
+		stage.setPoint(3, Vector2f(500.f, 780.f));
+		stage.setFillColor(Color(255, 32, 32, 255));
+	}
+
+	void SetStage(int setForce)
+	{
+		stage.setPoint(2, Vector2f(520.f, 780 - setForce));
+		stage.setPoint(3, Vector2f(500.f, 780 - setForce));
+	}
+};
+
+class MyText
+{
+public:
+
+	Text text;
+
+	MyText(int size, int r, int g, int b, int a, float sX, float sY)
+	{
+
+		text.setFont(font);
+		text.setCharacterSize(size);
+		text.setFillColor(Color(r, g, b, a));
+		text.setPosition(sX, sY);
+	}
+
+	MyText(int size, int r, int g, int b, int a)
+	{
+
+		text.setFont(font);
+		text.setCharacterSize(size);
+		text.setFillColor(Color(r, g, b, a));
+	}
+};
 
 int main()
 {
 	ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	RenderWindow window(VideoMode(600, 800), "Petank", Style::Close, settings);
+	RenderWindow window(VideoMode(600, 800), "Petank Game", Style::Close, settings);
+
+	float lowline, currtime = 0, velocity0 = 0;
+	int setForce = 0, angle = 0, count1 = 0, count2 = 0;
+	char str[10], str2[10], str3[10], str4[10];
+	bool hit = false, stop = false, winner = false;
 
 	std::srand(time(NULL));
-	float lowline = 300.f + (float)(std::rand() % 100), setAngle = 0.f, x = 180.f, y = 720.f, addPosX = 0, addPosY = 0, currtime = 0, velocity0 = 0, velocityX = 0;
-	int angle = 0, stop = 1, setForce = 0, azimuth = 0, count1 = 0, count2 = 0, winner = 0;
-	char str[4], str2[4], str7[3], str8[3];
-	bool hit = 0, ballStop = 0, ballStop2 = 0, player = 0, step1 = 0, step2 = 0;
+	lowline = 280.f + (float)(std::rand() % 120);
 
-	CircleShape ball(20.f);
-	ball.setFillColor(Color(24, 15, 61, 255));
-	ball.setPosition(x, y);
-
-	CircleShape ball2(20.f);
-	ball2.setFillColor(Color(206, 93, 34, 255));
-	ball2.setPosition(x, y);
-
-	Font font;
 	font.loadFromFile("C:/Windows/Fonts/arial.ttf");
-	Text txtAngle;
-	txtAngle.setFont(font);
-	txtAngle.setPosition(443.f, 753.f);
-	txtAngle.setCharacterSize(14);
-	txtAngle.setFillColor(Color(255, 255, 255, 255));
 
-	Text txtForce;
-	txtForce.setFont(font);
-	txtForce.setCharacterSize(14);
-	txtForce.setFillColor(Color(255, 255, 255, 255));
+	Ball BallB(20.f);
+	BallB.SetColor(163, 4, 251, 255);
+	BallB.SetStartPosition();
 
-	Text win;
-	win.setFont(font);
-	win.setPosition(413.f, 320.f);
-	win.setCharacterSize(18);
-	win.setFillColor(Color(28, 28, 28, 255));
-	win.setStyle(Text::Bold);
+	Ball BallO(20.f);
+	BallO.SetColor(253, 162, 2, 255);
+	BallO.SetStartPosition();
 
-	Text countB;
-	countB.setFont(font);
-	countB.setPosition(485.f, 280.f);
-	countB.setCharacterSize(24);
-	countB.setFillColor(Color(24, 15, 61, 255));
-	countB.setStyle(Text::Bold);
+	MyText txtAngle(14, 255, 255, 255, 255, 443.f, 753.f);
+	MyText txtForce(14, 255, 255, 255, 255);
+	MyText txtWin(18, 28, 28, 28, 255, 413.f, 320.f);
+	txtWin.text.setStyle(Text::Bold);
 
-	Text countO;
-	countO.setFont(font);
-	countO.setPosition(515.f, 280.f);
-	countO.setCharacterSize(24);
-	countO.setFillColor(Color(206, 93, 34, 255));
-	countO.setStyle(Text::Bold);
+	MyText txtCountB(24, 24, 15, 61, 255, 485.f, 280.f);
+	txtCountB.text.setStyle(Text::Bold);
+	MyText txtCountO(24, 206, 93, 34, 255, 515.f, 280.f);
+	txtCountO.text.setStyle(Text::Bold);
 
-	RectangleShape checkplus1(Vector2f(2.f, 2.f));
-	checkplus1.move(425.f, 707.f);
-	checkplus1.setFillColor(Color(255, 255, 255, 255));
+	StaticFigures checkplus1(2.f, 2.f, 255, 255, 255, 255, 425.f, 707.f);
+	StaticFigures checkplus2(2.f, 2.f, 255, 255, 255, 255, 475.f, 707.f);
+	StaticFigures checkplus3(1.f, 3.f, 255, 255, 255, 255, 450.f, 695.f);
 
-	RectangleShape checkplus2(Vector2f(2.f, 2.f));
-	checkplus2.move(475.f, 707.f);
-	checkplus2.setFillColor(Color(255, 255, 255, 255));
+	StaticFigures status(190.f, 130.f, 65, 61, 230, 255, 410.f, 670.f);
 
-	RectangleShape checkplus3(Vector2f(3.f, 3.f));
-	checkplus3.move(449.f, 700.f);
-	checkplus3.setFillColor(Color(255, 255, 255, 255));
+	StaticFigures border1(10.f, 790.f, 222, 227, 53, 255, 0.f, 10.f);
+	StaticFigures border2(410.f, 10.f, 222, 227, 53, 255, 0.f, 0.f);
+	StaticFigures border3(10.f, 790.f, 222, 227, 53, 255, 400.f, 10.f);
 
-	RectangleShape status(Vector2f(190.f, 130.f));
-	status.setFillColor(Color(65, 61, 230, 255));
-	status.setPosition(410.f, 670.f);
-
-	ConvexShape stage;
-	
+	ForceStage stage;
 
 	RectangleShape mainArea(Vector2f(400.f, lowline));
 	mainArea.setFillColor(Color(47, 137, 54, 224));
 
-	RectangleShape border1(Vector2f(10.f, 790.f));
-	border1.setFillColor(Color(222, 227, 53, 236));
-	border1.setPosition(0.f, 10.f);
+	Azimuth Line;
 
-	RectangleShape border2(Vector2f(410.f, 10.f));
-	border2.setFillColor(Color(222, 227, 53, 236));
+	Ball koshonet(8.f);
+	koshonet.SetColor(255, 219, 88, 255);
+	koshonet.SetKoshPosition();
 
-	RectangleShape border3(Vector2f(10.f, 790.f));
-	border3.setFillColor(Color(222, 227, 53, 236));
-	border3.setPosition(400.f, 10.f);
-
-	RectangleShape azimuthLine(Vector2f(50.f, 1.f));
-	azimuthLine.move(450.f, 750.f);
-
-	CircleShape koshonet(8.f, 30);
-	koshonet.setFillColor(Color(255, 219, 88, 255));
-	koshonet.setPosition(192.f, 170.f);
+	BallB.flag = true;
+	BallO.flag = false;
+	BallB.step = false;
+	BallO.step = false;
 
 	while (window.isOpen())
 	{
@@ -123,9 +239,9 @@ int main()
 
 				else if (event.key.code == Keyboard::Down) setForce -= 1;
 
-				else if (event.key.code == Keyboard::Left) azimuth += 1;
+				else if (event.key.code == Keyboard::Left) angle += 1;
 
-				else if (event.key.code == Keyboard::Right) azimuth -= 1;
+				else if (event.key.code == Keyboard::Right) angle -= 1;
 
 				else if (event.key.code == Keyboard::Space)
 				{
@@ -134,209 +250,149 @@ int main()
 			}
 		}
 
-		window.clear(Color(252, 218, 197, 255));
+		window.clear(Color(3, 252, 163, 255));
 
 		// грницы изменения угла
-		if (azimuth >= 30)
-		{
-			azimuth = 30;
-		}
-		if (azimuth <= -30)
-		{
-			azimuth = -30;
-		}
-		azimuthLine.setRotation(- 90 - azimuth);
-		azimuthLine.setFillColor(Color(255, 255, 255, 255));
+		if (angle >= 30) angle = 30;
+		else if (angle <= -30) angle = -30;
+
+		Line.SetAzimuth(angle);
 
 		// границы изменения скорости
-		if (setForce >= 100)
-		{
-			setForce = 100;
-		}
-		if (setForce <= 0)
-		{
-			setForce = 0;
-		}
-		velocity0 = setForce / 10 + 0.1 * (float)(setForce % 10);
+		if (setForce >= 100) setForce = 100;
+		else if (setForce <= 0) setForce = 0;
+		velocity0 = (float)setForce / 10 + 0.1 * (float)(setForce % 10);
 
 		// установка шкалы силы броска
-		stage.setPointCount(4);
-		stage.setPoint(0, Vector2f(500.f, 780.f));
-		stage.setPoint(1, Vector2f(520.f, 780.f));
-		stage.setPoint(2, Vector2f(520.f, 780.f - setForce));
-		stage.setPoint(3, Vector2f(500.f, 780.f - setForce));
-		stage.setFillColor(Color(255, 32, 32, 255));
-
-		//просчет движения шариков
-		if (player == 0)
+		stage.SetStage(setForce);
+		//обработка движения
+		if (BallB.flag)
 		{
-			if (hit == 1)
+			if (hit)
 			{
 				currtime += 0.016;
-				if (velocityX >= 0)
+				BallB.Move(currtime, angle, velocity0);
+				if (BallB.CurrVelocity(velocity0, currtime) <= 0)
 				{
-					
-					velocityX = (velocity0 - (2.2 * currtime * currtime / 2));
-					addPosX = velocityX * sin(azimuth * PI / 180);
-					addPosY = velocityX * cos(azimuth * PI / 180);
-					ball.move(-addPosX, -addPosY);
-				}
-				else
-				{
-					velocityX = 0;
 					hit = 0;
 					currtime = 0;
 					setForce = 0;
-					azimuth = 0;
-					ballStop = 0;
-					player = 1;
-					step1 = 1;
+					angle = 0;
+					Line.AzLine.setRotation(angle);
+					BallB.step = true;
+					BallB.flag = false;
+					BallO.flag = true;
 				}
-			} 
-			if ((ball.getPosition().x >= 400.f) || (ball.getPosition().x < -40.f) || (ball.getPosition().y < -40.f))
-			{
-				velocityX = 0;
-				hit = 0;
-				currtime = 0;
-				setForce = 0;
-				azimuth = 0;
-				ballStop = 1;
-				player = 1;
-				step1 = 1;
 			}
 		}
 
-		if(player == 1)
+		if (BallO.flag)
 		{
-			if (hit == 1)
+			if (hit)
 			{
 				currtime += 0.016;
-				if (velocityX >= 0)
+				BallO.Move(currtime, angle, velocity0);
+				if (BallO.CurrVelocity(velocity0, currtime) <= 0)
 				{
-
-					velocityX = (velocity0 - (2.2 * currtime * currtime / 2));
-					addPosX = velocityX * sin(azimuth * PI / 180);
-					addPosY = velocityX * cos(azimuth * PI / 180);
-					ball2.move(-addPosX, -addPosY);
-				}
-				else
-				{
-					velocityX = 0;
 					hit = 0;
 					currtime = 0;
 					setForce = 0;
-					azimuth = 0;
-					ballStop2 = 0;
-					player = 0;
-					step2 = 1;
+					angle = 0;
+					Line.AzLine.setRotation(angle);
+					BallO.step = true;
+					BallO.flag = false;
+					BallB.flag = true;
 				}
 			}
-			if ((ball2.getPosition().x >= 400.f) || (ball2.getPosition().x < -40.f) || (ball2.getPosition().y < -40.f))
-			{
-				velocityX = 0;
-				hit = 0;
-				currtime = 0;
-				setForce = 0;
-				azimuth = 0;
-				ballStop2 = 1;
-				player = 0;
-				step2 = 1;
-			}
 		}
-		
-		//подсчет очков и переход на следующий раунд
-		if ((step1 == 1) && (step2 == 1))
+		// подсчет очков и переход на следующий раунд
+		if (BallB.step && BallO.step)
 		{
-			if (sqrtf(pow(ball.getPosition().x + ball.getRadius() - koshonet.getPosition().x + koshonet.getRadius(), 2.f) + pow(ball.getPosition().y + ball.getRadius() - koshonet.getPosition().y + koshonet.getRadius(), 2.f)) < sqrtf(pow(ball2.getPosition().x + ball2.getRadius() - koshonet.getPosition().x + koshonet.getRadius(), 2.f) + pow(ball2.getPosition().y + ball2.getRadius() - koshonet.getPosition().y + koshonet.getRadius(), 2.f)))
+			if (BallB.distance(koshonet) < BallO.distance(koshonet))
 			{
-				if(ballStop == 0 && ball.getPosition().y < lowline)
-				count1++;
+				if (!BallB.CheckOut() && BallB.CheckLowline(lowline)) count1++;
 			}
-			else if (sqrtf(pow(ball.getPosition().x + ball2.getRadius() - koshonet.getPosition().x + koshonet.getRadius(), 2.f) + pow(ball.getPosition().y + ball2.getRadius() - koshonet.getPosition().y + koshonet.getRadius(), 2.f)) > sqrtf(pow(ball2.getPosition().x + +ball2.getRadius() - koshonet.getPosition().x + koshonet.getRadius(), 2.f) + pow(ball2.getPosition().y + +ball2.getRadius() - koshonet.getPosition().y + koshonet.getRadius(), 2.f)))
+			else if (BallB.distance(koshonet) > BallO.distance(koshonet))
 			{
-				if(ballStop2 == 0 && ball2.getPosition().y < lowline)
-				count2++;
+				if (!BallO.CheckOut() && BallO.CheckLowline(lowline)) count2++;
 			}
 			else
 			{
-				if (ballStop == 0 && ballStop2 == 0 && ball.getPosition().y < lowline && ball2.getPosition().y < lowline)
+				if (!BallB.CheckOut() && !BallO.CheckOut())
 				{
 					count1++;
 					count2++;
 				}
 			}
 
-			_itoa_s(count1, str7, 10);
-			countB.setString(str7);
-			_itoa_s(count2, str8, 10);
-			countO.setString(str8);
-			step1 = 0;
-			step2 = 0;
-			ball.setPosition(x, y);
-			ball2.setPosition(x, y);
-			ballStop = 0;
-			ballStop2 = 0;
+			_itoa_s(count1, str3, 10);
+			txtCountB.text.setString(str3);
+			_itoa_s(count2, str4, 10);
+			txtCountO.text.setString(str4);
+			BallB.step = false;
+			BallO.step = false;
+			BallB.SetStartPosition();
+			BallO.SetStartPosition();
 		}
 
 		if (count1 == 13)
 		{
-			win.setString("Blue Player win!");
-			winner = 1;
+			txtWin.text.setString("First Player win!");
+			winner = true;
 		}
-		else if(count2 == 13)
+		else if (count2 == 13)
 		{
-			win.setString("Orange Player win!");
-			winner = 1;
+			txtWin.text.setString("Second Player win!");
+			winner = true;
 		}
 
-		_itoa_s(azimuthLine.getRotation() - 270, str, 10);
-		txtAngle.setString(str);
+		_itoa_s(Line.AzLine.getRotation() - 270, str, 10);
+		txtAngle.text.setString(str);
 		_itoa_s(setForce, str2, 10);
-		txtForce.setString(str2);
-		txtForce.setPosition(530.f, 773.f - setForce);
-		
+		txtForce.text.setString(str2);
+		txtForce.text.setPosition(530.f, 773.f - setForce);
+
 		// отрисовка объектов
 		window.draw(mainArea);
-		window.draw(status);
-		window.draw(border1);
-		window.draw(border2);
-		window.draw(koshonet);
-		window.draw(border3);
-		if (ballStop2 == 0)
+		window.draw(status.SFigure);
+		window.draw(border1.SFigure);
+		window.draw(border2.SFigure);
+		window.draw(border3.SFigure);
+		window.draw(koshonet.ball);
+		if (!BallO.CheckOut())
 		{
-			window.draw(ball2);
+			window.draw(BallO.ball);
 		}
-		if (ballStop == 0)
+		if (!BallB.CheckOut())
 		{
-			window.draw(ball);
+			window.draw(BallB.ball);
 		}
-		window.draw(txtAngle);
-		window.draw(txtForce);
-		window.draw(stage);
-		window.draw(azimuthLine);
-		window.draw(checkplus1);
-		window.draw(checkplus2);
-		window.draw(checkplus3);
-		window.draw(countB);
-		window.draw(countO);
-		if (winner == 1)
+		window.draw(txtAngle.text);
+		window.draw(txtForce.text);
+		window.draw(stage.stage);
+		window.draw(Line.AzLine);
+		window.draw(checkplus1.SFigure);
+		window.draw(checkplus2.SFigure);
+		window.draw(checkplus3.SFigure);
+		window.draw(txtCountB.text);
+		window.draw(txtCountO.text);
+		if (winner)
 		{
-			window.draw(win);	
-			stop = 0;
+			window.draw(txtWin.text);
+			stop = true;
 		}
-
 
 		// задержка обновления экрана
 		std::this_thread::sleep_for(std::chrono::milliseconds(16));
 		window.display();
 
 		// обработка закрытия программы
-		if (stop == 0)
+		if (stop)
 		{
 			std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-			win.setString("Game is over.\nWait 5 seconds.");
-			win.move(0.f, 30.f);
-			window.draw(win);
+			txtWin.text.setString("Game is over.\nWait 5 seconds.");
+			txtWin.text.move(0.f, 30.f);
+			window.draw(txtWin.text);
 			window.display();
 			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 			std::exit(0);
